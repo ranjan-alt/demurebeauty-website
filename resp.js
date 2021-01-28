154228596f96df9a759dd5abad093182db6101b3
Ranjan Kumar <67799181+ranjan-alt@users.noreply.github.com>
2021-01-28 14:24:58 +0530

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,16 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+rightnav = document.querySelector('.rightnav')
+navlist = document.querySelector('.navlist')
+
+
+
+
+
+
+burger.addEventListener('click', () => {
+    navbar.classList.toggle('v-class-resp');
+    navlist.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+
+})
